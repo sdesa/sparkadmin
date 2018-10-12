ad0d3aa707eaccf08fb3ac636a9eb6b3f5e3a14e
Siva <siva.desaraju@vizientinc.com>
2018-10-12 00:55:11 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sparkadmin';
+  signedIn: boolean;
+  user: any;
+  greeting: string;
+
+  constructor( private amplifyService: AmplifyService ) {
+
+    this.amplifyService.authStateChange$
+        .subscribe(authState => {
+            this.signedIn = authState.state === 'signedIn';
+            if (!authState.user) {
+                this.user = null;
+            } else {
+                this.user = authState.user;
+                this.greeting = "Hello " + this.user.username;
+            }
+    });    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { LoginComponent } from './login/login.component';
+
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmplifyAngularModule,
+    MaterialModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AmplifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
